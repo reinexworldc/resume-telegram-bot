c3b7790fe7808f46bb9ee8a7690c7cdf1e1b13da
exworldcreator <rydanh400@gmail.com>
2025-03-13 03:53:56 +0300

    Improved local resume verification with more loyal criteria

--- run.py
+++ run.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 """
-Скрипт для запуска бота.
+Скрипт для запуска бота из корневой директории проекта.
 """
 
 import asyncio
+import logging
 from src.bot import main
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
     asyncio.run(main()) 
